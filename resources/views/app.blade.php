abf8c1ae1c57835c47ca65cef7eef33e0a69a6c5
chikbob <ricarrdo1488@gmail.com>
2024-03-09 14:15:07 +0300

    creating new project with vue+laravel+inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> <html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <script src="https://unpkg.com/vue"></script>
+    <script src="https://unpkg.com/vue@next/dist/vue.global.prod.js"></script>
+    <link rel="icon" href="" type="image/x-icon">
+    @vite('resources/js/app.js')
+    @inertiaHead
+
+    <title>College of Technology</title>
+</head>
+
+<body>
+@inertia
+</body>
+
+</html>
